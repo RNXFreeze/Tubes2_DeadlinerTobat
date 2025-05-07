75bab48fa9488f4cc24cd0a2f41d8de4ff9bf0f6
anellautari <19623229@std.stei.itb.ac.id>
2025-05-08 01:08:10 +0700

    feat: add front-end

---
+++ src/front-end/components/navbar.js
@@ -0,0 +1,16 @@
+// components/Navbar.js
+export default function Navbar() {
+    return (
+      <nav className="bg-purple-400 text-white p-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">Deadliner Tobat</h1>
+          <ul className="flex space-x-4">
+            <li><a href="#" className="hover:underline">Home</a></li>
+            <li><a href="#" className="hover:underline">About</a></li>
+            <li><a href="#" className="hover:underline">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  
