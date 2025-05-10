2930ebe623273c9f955082a7d37c7259a51c7359
anellautari <19623229@std.stei.itb.ac.id>
2025-05-10 23:53:55 +0700

    feat: modify layout 1

---
+++ src/front-end/tampilan1/app/page.js
@@ -0,0 +1,40 @@
+'use client'
+
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import RecipeToggle from '../components/RecipeToggle';
+import AlgorithmDropdown from '../components/AlgorithmDropdown';
+import RecipeSlider from '../components/RecipeSlider';
+
+
+export default function Home() {
+  const [recipeType, setRecipeType] = useState('multiple');
+  const [algorithmType, setAlgorithm] = useState('BFS');
+  const [element,setElement] = useState('');
+  const [maxRecipe, setMaxRecipe] = useState('10');
+
+  return (
+    <div className="bg-purple-100 min-h-screen">
+      <Navbar />
+      <main className="flex flex-col items-center justify-center p-6 gap-6 text-center">
+        <h1 
+          className="text-3xl font-bold text-center">
+            Little Alchemy Recipe Finder
+        </h1>
+        <RecipeToggle recipeType={recipeType} setRecipeType={setRecipeType} />
+        <AlgorithmDropdown
+          algorithmType={algorithmType}
+          setAlgorithm={setAlgorithm}
+          element={element}
+          setElement={setElement}
+          handleSearch={() => {
+            console.log("Search clicked:", element, algorithmType);
+          }}
+        />
+      {recipeType === 'multiple' && (
+        <RecipeSlider value={maxRecipe} setValue={setMaxRecipe} />
+      )}
+      </main>
+    </div>
+  );
+}
---
+++ src/front-end/tampilan1/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+      <nav className="bg-purple-400 text-white p-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">Deadliner Tobat</h1>
+          <ul className="flex space-x-4">
+            <li><a href="#" className="hover:underline">Tubes 2 IF2210 Strategi Algoritma</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  
