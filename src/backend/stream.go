269d095f77ec9ad8bcf829c11346f659364e2fc2
anellautari <19623229@std.stei.itb.ac.id>
2025-05-12 23:26:56 +0700

    feat: update func BFSstream

--- src/backend/stream.go
+++ src/backend/stream.go
@@ -1,27 +1,9 @@
 package backend
 
 func BFSStream(gallery *Gallery, target string, maxRecipe int, out chan<- *RecipeNode) {
-	visited := make(map[string]bool)
-	type queueItem struct {
-		Node *RecipeNode
-	}
+    res := BFS(gallery, target, maxRecipe)
 
-	queue := []*RecipeNode{}
-	res := BFS(gallery, target, maxRecipe)
-	queue = append(queue, res.Trees...)
-
-	for len(queue) > 0 {
-		cur := queue[0]
-		queue = queue[1:]
-
-		if visited[cur.Name] {
-			continue
-		}
-		visited[cur.Name] = true
-		out <- cur
-
-		for _, parent := range cur.Parents {
-			queue = append(queue, parent)
-		}
-	}
+    for _, tree := range res.Trees {
+        out <- tree // kirim 1 tree utuh per sekali kirim
+    }
 }
